359e41ad282c7f74d6f7a8a4c1a91497853f8d23
Sean <kingsora999@gmail.com>
2024-01-16 22:28:09 -0800

    dev w database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const postgres = require('postgres');
 require('dotenv').config();
 const quoteController = require('./controllers/quoteController.js');
+
+app.get('/entries', (req, res) => {
+  console.log('inside the database');
+  return res.status(200).json();
+});
 
 app.get('/api/quote', quoteController.getSingleQuote, (req, res) => {
   console.log('back to frontend:', res.locals.quote);
   return res.status(200).json(res.locals.quote);
 });
 
-app.listen(3000, () => console.log(`Server running on port 3000`));
+app.listen(8080, () => console.log(`Server running on port 3000`));
