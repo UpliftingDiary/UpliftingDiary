4232fefee6a7f07f4467a89355fb84280838fdef
Rose <rhoswen827@gmail.com>
2024-01-16 20:36:52 -0500

    rendered quote from external API and simple UI setup

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,11 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
+import styles from './styles.scss';
+
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
